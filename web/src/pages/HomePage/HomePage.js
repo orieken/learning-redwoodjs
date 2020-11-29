8be162f3f1e9bfe0436b6e5e65d50a5262b8f2d4
Rieken <oscar.rieken@adp.com>
2020-11-29 11:56:11 -0500

    :sparkles: (Tutorial) #2 blog posts

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,7 +1,12 @@
 import BlogLayout from 'src/layouts/BlogLayout/BlogLayout';
+import BlogPostsCell from 'src/components/BlogPostsCell'
 
 const HomePage = () => {
-  return <BlogLayout>Home</BlogLayout>
-}
+  return (
+      <BlogLayout>
+        <BlogPostsCell />
+      </BlogLayout>
+  );
+};
 
-export default HomePage
+export default HomePage;
